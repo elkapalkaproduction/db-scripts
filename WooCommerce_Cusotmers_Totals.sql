cdf30921dc45f53c07d41a6016509bfe43820a95
Pavel Mamaev <elkapalkaproduction@gmail.com>
2017-01-22 14:10:49 -0800

    Update 1.0
    Took out DB name from the script

--- WooCommerce_Cusotmers_Totals.sql
+++ WooCommerce_Cusotmers_Totals.sql
@@ -6,17 +6,17 @@ FROM (Select ou_final.user_id, sum(ou_final.order_total) as total_spent, count(d
 FROM 
 (Select ou.order_id, ou.user_id, ot.order_total
 FROM 
-(Select pm.post_id as order_id, pm.meta_value as user_id from avtoram.wp_postmeta pm
+(Select pm.post_id as order_id, pm.meta_value as user_id from wp_postmeta pm
 where pm.post_id in
 (SELECT pm.post_id 
-FROM avtoram.wp_postmeta pm WHERE pm.meta_key = '_order_total' and pm.meta_value > 0) 
+FROM wp_postmeta pm WHERE pm.meta_key = '_order_total' and pm.meta_value > 0) 
 and pm.meta_key = '_customer_user') as ou
 INNER JOIN 
 (SELECT pm.post_id AS order_id, pm.meta_value AS order_total 
-FROM avtoram.wp_postmeta pm WHERE pm.meta_key = '_order_total' and pm.meta_value > 0) as ot
+FROM wp_postmeta pm WHERE pm.meta_key = '_order_total' and pm.meta_value > 0) as ot
 ON
 ou.order_id = ot.order_id)  as ou_final
 group by ou_final.user_id) as payers
-inner join avtoram.wp_users us
+inner join wp_users us
 ON payers.user_id = us.ID
 order by payers.total_spent DESC
